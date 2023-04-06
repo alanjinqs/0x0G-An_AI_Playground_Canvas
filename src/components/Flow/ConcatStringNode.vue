1865796cbb6c44da5f86c43982a1e6c88aecbbcf
Alan Jin <o@alanj.in>
2023-04-06 13:27:54 +0800

    feat: 🎸 (Concat string) node now have 4 sizes

--- src/components/Flow/ConcatStringNode.vue
+++ src/components/Flow/ConcatStringNode.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { Handle, Position } from "@vue-flow/core"
+import { ref } from "vue"
 
 defineProps({
   data: {
@@ -33,11 +34,27 @@ const emits = defineEmits(["freeze", "defreeze"])
     <div class="card bg-neutral">
       <div class="card-body">
         <h2 class="card-title">String</h2>
+        <div class="flex items-center">
+          <span class="mb-0.5">Size: </span>
+          <button @click="data.data.size = 'xs'" class="btn btn-xs">xs</button>
+          <button @click="data.data.size = 'sm'" class="btn btn-xs">sm</button>
+          <button @click="data.data.size = 'md'" class="btn btn-xs">md</button>
+          <button @click="data.data.size = 'lg'" class="btn btn-xs">lg</button>
+        </div>
         <textarea
           @focus="emits('freeze')"
           @blur="emits('defreeze')"
           v-model="data.data.stringTemplate"
-          class="textarea resize-none hover:resize-y textarea-sm w-80 h-60 leading-tight"
+          :class="[
+            'textarea resize-none textarea-sm leading-tight',
+            data.data.size == 'xs'
+              ? 'w-full h-8'
+              : data.data.size == 'sm'
+              ? 'w-full h-16'
+              : data.data.size == 'lg'
+              ? 'w-80 h-60 hover:resize-y '
+              : 'w-full h-32'
+          ]"
         ></textarea>
       </div>
     </div>
@@ -67,6 +84,7 @@ const emits = defineEmits(["freeze", "defreeze"])
     style="top: 150px; bottom: auto"
   />
   <Handle
+  v-if="data.data.size == 'lg'"
     id="str-in-4"
     :connectable="true"
     :connectable-start="false"
